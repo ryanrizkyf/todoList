889f13463404b8fbb5754e973f7043f201110df1
Ryan <ryanrizkyf@gmail.com>
2020-02-08 14:48:30 +0700

    inisialisasi todolist app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Todo from "./components/Todo";
+import AddTodo from "./components/AddTodo";
+import Header from "./components/Header";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  state = {
+    todos: [],
+    isLoading: false
+  };
+
+  componentDidMount(){
+    const context = this
+    this.setState({isLoading: true}, () => axios
+    .get("http://mysterious-coast-83531.herokuapp.com/todos")
+    .then(res => context.setState({
+      todos: res.data,
+      isLoading: false
+    })
+    )
+    )
+  }
+
+  //toggle complete
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo._id === id) {
+          todo.status = !todo.status;
+        }
+        return todo;
+      })
+    });
+  };
+
+  //Add Todo
+  AddTodo = kegiatan => {
+    axios
+    .post("http://mysterious-coast-83531.herokuapp.com/todos", {
+      kegiatan,
+      status: false
+    })
+    .then(res =>
+      this.setState({
+        todos: [...this.state.todos, res.data]
+      })
+    );
+  };
+
+  render(){
+    return(
+      <div className="App">
+        <div className="container">
+          <Header />
+          <AddTodo AddTodo={this.AddTodo} />{" "}
+          {this.state.isLoading? <div>Lagi Loading...</div>:<Todo
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+        />}
+        </div>{" "}
+      </div>
+    );
+  }
 }
 
 export default App;
